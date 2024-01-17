c882c6a49e634099c34624569a8af65f3ee57e1e
Martin Chavez <martinchavez1203@gmail.com>
2024-01-17 11:52:04 -0700

    US 3 complete

--- spec/features/airlines/show_spec.rb
+++ spec/features/airlines/show_spec.rb
@@ -1,25 +1,24 @@
-require "rails_helper"
+require 'rails_helper'
+RSpec.describe "airlines passangers" do 
 
-RSpec.describe Airline, type: :model do
-  describe "relationships" do
-    it {should have_many :flights}
-  end
-
-  it 'returns distinct adult passengers' do
+  it 'returns a list of unique adult passengers' do
     airline = Airline.create(name: 'Example Airline')
-
+  
     adult_passenger1 = Passenger.create(name: 'Adult Passenger 1', age: 25)
     adult_passenger2 = Passenger.create(name: 'Adult Passenger 2', age: 30)
     child_passenger = Passenger.create(name: 'Child Passenger', age: 10)
-
+  
     flight1 = Flight.create(number: 'FL123', date: '2024-01-20', airline: airline)
     flight2 = Flight.create(number: 'FL456', date: '2024-01-21', airline: airline)
-
+  
     flight1.passengers << [adult_passenger1, child_passenger]
     flight2.passengers << [adult_passenger2, child_passenger]
-
-    result = airline.adult_passengers
-
-    expect(result).to contain_exactly(adult_passenger1, adult_passenger2)
+  
+    unique_adult_passengers = airline.adult_passengers
+  
+    expect(unique_adult_passengers).to include(adult_passenger1)
+    expect(unique_adult_passengers).to include(adult_passenger2)
+    expect(unique_adult_passengers).not_to include(child_passenger)
   end
+  
 end
